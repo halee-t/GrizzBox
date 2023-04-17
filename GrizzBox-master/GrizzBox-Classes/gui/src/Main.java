a66b8f45db1ca405bc9b243de23034484ced2405
denicara <123340232+denicara@users.noreply.github.com>
2023-04-16 21:14:34 -0400

    Scoreboard

---
+++ GrizzBox-master/GrizzBox-Classes/gui/src/Main.java
@@ -0,0 +1,32 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import static javafx.application.Application.launch;
+
+public class Main extends Application {
+    public void start(Stage stage) throws Exception {
+        FileInputStream s=  new FileInputStream("C:\\Users\\Deni\\IdeaProjects\\GrizzBox-master\\GrizzBox-Classes\\Start Screen.jpg");
+        Image image = new Image(s);
+        ImageView iv = new ImageView();
+        iv.setImage(image);
+
+        Parent root = FXMLLoader.load(getClass().getResource("start.fxml"));//load resources from fxml file
+        Scene scene = new Scene(root);//get scene from root
+        stage.setTitle("GrizzBox");//set title to grizzbox
+        stage.setScene(scene);//set scene
+        stage.show();//show stage
+
+    }
+    public static void main(String[] args) throws FileNotFoundException {
+        GrizzBox gb = new GrizzBox();
+        launch(args);
+    }
+}
